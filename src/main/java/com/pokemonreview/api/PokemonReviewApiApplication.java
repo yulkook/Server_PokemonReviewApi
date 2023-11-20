13f5439f09f7604d04e4c2014176b86b84ee7954
yulkook <paullee@yulkook.org>
2023-11-20 13:41:52 +0900

    initial commit

---
+++ src/main/java/com/pokemonreview/api/PokemonReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokemonreview/api/PokemonReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
